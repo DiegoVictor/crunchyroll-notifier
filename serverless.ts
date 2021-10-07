70f1957b811244725305068824094509fd6586b6
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-06 21:44:52 -0300

    Create serverless project

---
+++ serverless.ts
@@ -0,0 +1,29 @@
+import type { AWS } from "@serverless/typescript";
+
+
+const serverlessConfiguration: AWS = {
+  service: "crunchyroll-notifier",
+  frameworkVersion: "2",
+  custom: {
+    webpack: {
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
+  },
+  provider: {
+    name: "aws",
+    runtime: "nodejs14.x",
+    region: "us-east-1",
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      EXECUTION_INTERVAL_MINUTES: "15",
+    },
+    lambdaHashingVersion: "20201221",
+  },
+};
+
+module.exports = serverlessConfiguration;
