98c07c4d1e3f9c15545e19e024d461c864604204
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-07 21:10:31 -0300

    Get RSS XML

--- src/functions/rss/handler.ts
+++ src/functions/rss/handler.ts
@@ -4,6 +4,9 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 const rss = async (event: APIGatewayProxyEvent) => {
   try {
     const { date = new Date() } = event.queryStringParameters || {};
+    const { data: rss } = await axios.get(
+      "http://feeds.feedburner.com/crunchyroll/rss/anime?lang=ptBR"
+    );
     return formatJSONResponse(204);
   } catch (err) {
     return formatJSONResponse(500, err.message);
