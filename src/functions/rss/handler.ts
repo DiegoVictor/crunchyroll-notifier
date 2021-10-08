e160388042e38c74c542a4baac45e9169fa14649
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-07 21:10:10 -0300

    Read optional date parameter

--- src/functions/rss/handler.ts
+++ src/functions/rss/handler.ts
@@ -2,6 +2,12 @@ import "source-map-support/register";
 
 import { APIGatewayProxyEvent } from "aws-lambda";
 const rss = async (event: APIGatewayProxyEvent) => {
+  try {
+    const { date = new Date() } = event.queryStringParameters || {};
+    return formatJSONResponse(204);
+  } catch (err) {
+    return formatJSONResponse(500, err.message);
+  }
 };
 
 export const main = middyfy(rss);
