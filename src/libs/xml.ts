afb05503b9356b60fc905ae4b0fb02baadbf0707
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-10 14:11:46 -0300

    Search node by name

--- src/libs/xml.ts
+++ src/libs/xml.ts
@@ -1,7 +1,27 @@
 import * as parser from "xml-js";
 
+import { Node } from "src/application/contracts/Node";
+
 export const toJS = (xml: string) => {
   return parser.xml2js(xml, {
     compact: false,
   });
 };
+
+export function getNodeFrom(name: string, from: Node[]): Node | null {
+  const item = from.find((element) => element.name === name);
+
+  if (item && item.elements && item.elements.length > 0) {
+    return item.elements.pop();
+  }
+
+  if (name.includes("media") && item.attributes && item.attributes.url) {
+    return {
+      type: "element",
+      name,
+      text: item.attributes.url,
+    };
+  }
+
+  return null;
+}
