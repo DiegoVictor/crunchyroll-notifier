773a9934894b27b591473780f0f6a6fa817f064d
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-13 20:01:17 -0300

    Update download script

--- scripts/rss/download-feed.js
+++ scripts/rss/download-feed.js
@@ -1,6 +1,6 @@
-import axios from "axios";
-import * as fs from "fs";
+const axios = require("axios");
+const fs = require("fs");
 
 axios
   .get("http://feeds.feedburner.com/crunchyroll/rss/anime?lang=ptBR")
-  .then(({ data: rss }) => fs.promises.writeFile("feed.xml", rss));
+  .then(({ data: rss }) => fs.promises.writeFile(`${__dirname}/feed.xml`, rss));
