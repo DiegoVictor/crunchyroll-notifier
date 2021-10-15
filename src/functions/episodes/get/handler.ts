4b1a39e16e1f23fe73935f00d12b998459fa9e93
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-15 19:44:04 -0300

    Send response

--- src/functions/episodes/get/handler.ts
+++ src/functions/episodes/get/handler.ts
@@ -1,10 +1,12 @@
 import "source-map-support/register";
 import { APIGatewayProxyEvent } from "aws-lambda";
 
+import * as response from "@libs/response";
 export const get = async (event: APIGatewayProxyEvent) => {
   const { date = new Date() } = event.queryStringParameters || {};
 
   try {
+    return response.NoContent();
   } catch (err) {
     return response.InternalServerError(err);
   }
