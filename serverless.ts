1edb1f38d3a88996fe2439181fef4c6ce0008c2d
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-14 22:35:44 -0300

    Configure execution interval

--- serverless.ts
+++ serverless.ts
@@ -28,8 +28,7 @@ const serverlessConfiguration: AWS = {
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
-      EXECUTION_INTERVAL_MINUTES: "15",
-      LOCALSTACK_ENDPOINT: "http://localhost:4566",
+      EXECUTION_INTERVAL_MINUTES: "${self:custom.executionIntervalMinutes}",
     },
     lambdaHashingVersion: "20201221",
   },
