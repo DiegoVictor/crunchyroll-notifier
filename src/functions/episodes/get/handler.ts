c0ccdd7ce2328d29aa8ffa4f840af8097d2ab1e9
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-17 15:56:03 -0300

    Load new episodes

--- src/functions/episodes/get/handler.ts
+++ src/functions/episodes/get/handler.ts
@@ -2,10 +2,12 @@ import "source-map-support/register";
 import { APIGatewayProxyEvent } from "aws-lambda";
 
 import * as response from "@infra/libs/response";
+import { getNewEpisodes } from "@application/use_cases/getNewEpisodes";
 export const get = async (event: APIGatewayProxyEvent) => {
   const { date = new Date() } = event.queryStringParameters || {};
 
   try {
+    const episodes = await getNewEpisodes(date.toString());
     return response.NoContent();
   } catch (err) {
     return response.InternalServerError(err);
