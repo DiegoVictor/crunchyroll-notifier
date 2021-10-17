13f6231d5fdb6c1742d9f1465ff0cf8cf626a8ab
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-17 15:51:19 -0300

    Get crunchyroll animes rss xml

--- src/application/use_cases/getNewEpisodes.ts
+++ src/application/use_cases/getNewEpisodes.ts
@@ -1,3 +1,6 @@
 import { Episode } from "@application/contracts/Episode";
+import { getAnimesRss } from "@infra/services/cruchyroll";
+
 export const getNewEpisodes = async (fromDate?: string): Promise<Episode[]> => {
+  const rss = await getAnimesRss();
 };
