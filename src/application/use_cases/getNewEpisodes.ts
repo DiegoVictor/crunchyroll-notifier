efbcc2e0b6a2d18dff0322cf765e25325579ee9b
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-17 15:53:19 -0300

    Get last rss update

--- src/application/use_cases/getNewEpisodes.ts
+++ src/application/use_cases/getNewEpisodes.ts
@@ -3,4 +3,12 @@ import { getAnimesRss } from "@infra/services/cruchyroll";
 
 export const getNewEpisodes = async (fromDate?: string): Promise<Episode[]> => {
   const rss = await getAnimesRss();
+
+  const { elements } = xml.toJS(rss);
+  const channel = xml.getNode("rss", elements);
+
+  const { text: lastBuildDate } = xml.getNode(
+    "lastBuildDate",
+    channel.elements
+  );
 };
