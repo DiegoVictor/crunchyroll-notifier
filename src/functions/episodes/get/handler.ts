b1c43bdfa60082d87e98bc141f48e1e820379520
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-18 19:20:37 -0300

    Send episodes to queue

--- src/functions/episodes/get/handler.ts
+++ src/functions/episodes/get/handler.ts
@@ -3,11 +3,17 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 
 import * as response from "@infra/libs/response";
 import { getNewEpisodes } from "@application/use_cases/getNewEpisodes";
+import { addEpisodesInQueue } from "@application/use_cases/addEpisodesInQueue";
+
 export const get = async (event: APIGatewayProxyEvent) => {
   const { date = new Date() } = event.queryStringParameters || {};
 
   try {
     const episodes = await getNewEpisodes(date.toString());
+    if (episodes.length > 0) {
+      await addEpisodesInQueue(episodes);
+    }
+
     return response.NoContent();
   } catch (err) {
     return response.InternalServerError(err);
