df40efd760ab8674ad63eb7ad40567858090011e
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-18 19:19:28 -0300

    Send message batch

--- src/infra/services/episodes.ts
+++ src/infra/services/episodes.ts
@@ -1,2 +1,15 @@
 import { SQS } from "@aws-sdk/client-sqs";
+import { randomUUID } from "crypto";
+
+import { Episode } from "@application/contracts/Episode";
 const sqs = new SQS({});
+
+export const sendMessageBatch = async (episodes: Episode[]) => {
+  await sqs.sendMessageBatch({
+    QueueUrl: process.env.QUEUE_URL,
+    Entries: episodes.map((episodes) => ({
+      Id: randomUUID(),
+      MessageBody: JSON.stringify(episodes),
+    })),
+  });
+};
