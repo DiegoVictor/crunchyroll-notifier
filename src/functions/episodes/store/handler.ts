5701166f1f1d9c1aab62ca4cd6a71444eecf9c57
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-19 18:58:57 -0300

    Return no content response

--- src/functions/episodes/store/handler.ts
+++ src/functions/episodes/store/handler.ts
@@ -1,9 +1,11 @@
 import { Episode } from "@application/contracts/Episode";
 import { Message } from "@infra/contracts/Message";
+import * as response from "@infra/libs/response";
 import { receiveMessages as receiveMessagesFromQueue } from "@infra/services/episodes";
 export const store = async () => {
   try {
     const messages: Message<Episode>[] = await receiveMessagesFromQueue();
+    return response.NoContent();
   } catch (err) {
     return response.InternalServerError(err);
   }
