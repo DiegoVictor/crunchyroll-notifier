6ded00318503f15182417b9353e8788eb762b9cb
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-19 18:58:27 -0300

    Get episodes' messages

--- src/functions/episodes/store/handler.ts
+++ src/functions/episodes/store/handler.ts
@@ -1,5 +1,9 @@
+import { Episode } from "@application/contracts/Episode";
+import { Message } from "@infra/contracts/Message";
+import { receiveMessages as receiveMessagesFromQueue } from "@infra/services/episodes";
 export const store = async () => {
   try {
+    const messages: Message<Episode>[] = await receiveMessagesFromQueue();
   } catch (err) {
     return response.InternalServerError(err);
   }
