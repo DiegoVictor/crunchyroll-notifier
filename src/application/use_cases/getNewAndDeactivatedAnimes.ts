ddbea23059b6c0781508d6ad949781897cb86c1d
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-21 20:20:46 -0300

    Get deactivated and new animes

--- src/application/use_cases/getNewAndDeactivatedAnimes.ts
+++ src/application/use_cases/getNewAndDeactivatedAnimes.ts
@@ -1,4 +1,6 @@
 import { Episode } from "@application/contracts/Episode";
+import { findByTitle as findAnimesByTitle } from "@infra/repositories/implementations/animes";
+import { mapFields } from "@application/parsers/animes";
 
 export const getNewAndDeactivatedAnimes = async (episodes: Episode[]) => {
   const titles: string[] = episodes.reduce((series, { serie }) => {
@@ -7,4 +9,15 @@ export const getNewAndDeactivatedAnimes = async (episodes: Episode[]) => {
     }
     return series;
   }, []);
+
+  return findAnimesByTitle(titles).then((saved) => {
+    const animes = titles
+      .filter((anime) => !saved.find(({ title }) => title === anime))
+      .map((title) => mapFields({ title }));
+
+    return {
+      animes,
+      deactivated: saved.filter(({ active }) => !active),
+    };
+  });
 };
--- src/functions/episodes/store/handler.ts
+++ src/functions/episodes/store/handler.ts
@@ -3,12 +3,18 @@ import { getFrom as getEpisodesFromMessages } from "@infra/services/episodes";
 import { Message } from "@infra/contracts/Message";
 import * as response from "@infra/libs/response";
 import { receiveMessages as receiveMessagesFromQueue } from "@infra/services/episodes";
+import { getNewAndDeactivatedAnimes } from "@application/use_cases/getNewAndDeactivatedAnimes";
+
 export const store = async () => {
   try {
     const messages: Message<Episode>[] = await receiveMessagesFromQueue();
 
     if (messages.length > 0) {
       const episodes = getEpisodesFromMessages(messages);
+      const { animes, deactivated } = await getNewAndDeactivatedAnimes(
+        episodes
+      );
+
     }
 
     return response.NoContent();
