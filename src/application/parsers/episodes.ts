425ace585822894c35f2236dd4f11215ea579bc4
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-25 18:32:31 -0300

    Update mapper

--- src/application/parsers/episodes.ts
+++ src/application/parsers/episodes.ts
@@ -2,7 +2,26 @@ import { randomUUID } from "crypto";
 
 import { Episode } from "@application/contracts/Episode";
 
-export const mapFields = (data: Episode): Episode =>
-  Object.assign({}, data, {
-    id: randomUUID(),
-  });
+export const mapFields = ({
+  id = randomUUID(),
+  serie,
+  title,
+  number,
+  season,
+  description,
+  thumbnail,
+  premiumPublishDate,
+  freePublishDate,
+}: {
+  [key: string]: string;
+}): Episode => ({
+  id,
+  serie,
+  title,
+  number,
+  season: Number(season),
+  description,
+  thumbnail,
+  premiumPublishDate: Number(premiumPublishDate),
+  freePublishDate: Number(freePublishDate),
+});
