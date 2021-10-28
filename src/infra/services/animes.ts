98c55b3d582bcf8bb134d9aaf5ab9d74b654dfeb
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-27 21:42:30 -0300

    Move to notifications service

--- src/infra/services/animes.ts
+++
@@ -1,7 +0,0 @@
-import { SNS } from "@aws-sdk/client-sns";
-
-const sns = new SNS({});
-
-export const createTopic = async (Name: string) => {
-  await sns.createTopic({ Name });
-};
--- src/infra/services/notifications.ts
+++ src/infra/services/notifications.ts
@@ -2,6 +2,11 @@ import { SNS } from "@aws-sdk/client-sns";
 import { Anime } from "@application/contracts/Anime";
 
 const sns = new SNS({});
+
+export const createTopic = async (Name: string) => {
+  await sns.createTopic({ Name });
+};
+
 export const sendSubscribeRequest = async ({ topic }: Anime) => {
   await sns.publish({
     Message: JSON.stringify({
