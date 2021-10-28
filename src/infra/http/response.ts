c7f005f08e9c87ce37e6492015569ce11aeec20b
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-27 21:50:46 -0300

    Return directly

--- src/infra/http/response.ts
+++ src/infra/http/response.ts
@@ -1,23 +1,17 @@
-export const NoContent = () => {
-  return {
-    statusCode: 204,
-    body: "",
-  };
-};
+export const NoContent = () => ({
+  statusCode: 204,
+  body: "",
+});
 
-export const OK = <T>(body: T) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(body),
-  };
-};
+export const OK = <T>(body: T) => ({
+  statusCode: 200,
+  body: JSON.stringify(body),
+});
 
-export const InternalServerError = () => {
-  return {
-    statusCode: 500,
-    body: JSON.stringify({
-      code: 500,
-      message: "Ops! Something goes wrong, try again later",
-    }),
-  };
-};
+export const InternalServerError = () => ({
+  statusCode: 500,
+  body: JSON.stringify({
+    code: 500,
+    message: "Ops! Something goes wrong, try again later",
+  }),
+});
