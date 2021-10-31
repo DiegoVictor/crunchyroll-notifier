cecd602f71c9238b836276f3310ba3381361ba51
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-10-31 00:38:11 -0300

    Create episodes table resource

--- serverless.ts
+++ serverless.ts
@@ -77,6 +77,31 @@ const serverlessConfiguration: AWS = {
           },
         },
       },
+      EpisodesTable: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "Episodes",
+          AttributeDefinitions: [
+            {
+              AttributeName: "id",
+              AttributeType: "S",
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: "id",
+              KeyType: "HASH",
+            },
+          ],
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+          },
+          StreamSpecification: {
+            StreamViewType: "NEW_IMAGE",
+          },
+        },
+      },
     },
   },
 };
