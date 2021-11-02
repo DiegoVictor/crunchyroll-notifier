4467b88b6a6420961f77d5cb245f9068cbf4ab89
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-11-02 17:40:22 -0300

    Create anime factory

--- tests/utils/factory.ts
+++ tests/utils/factory.ts
@@ -1,4 +1,16 @@
 import * as faker from "faker";
 import { factory } from "factory-girl";
 
+
+factory.define("Anime", {}, () => {
+  const title = faker.name.title();
+
+  return {
+    id: faker.datatype.uuid(),
+    active: true,
+    topic: title.replace(/(\W|_)/gi, ""),
+    title,
+  };
+});
+
 export { factory };
