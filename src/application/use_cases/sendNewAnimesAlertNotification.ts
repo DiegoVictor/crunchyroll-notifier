5d2a602db598379153b85cb1374fca2cdec09742
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-11-08 19:07:21 -0300

    Remove not utilized use cases

--- src/application/use_cases/sendNewAnimesAlertNotification.ts
+++
@@ -1,5 +0,0 @@
-import { Anime } from "@application/contracts/Anime";
-import { sendSubscribeRequest } from "@infra/services/notifications";
-
-export const sendNewAnimesAlertNotification = async (animes: Anime[]) =>
-  animes.map(sendSubscribeRequest);
