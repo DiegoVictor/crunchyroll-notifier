52a08e36927216c6c0beefc68a312f1d8ff21259
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-11-10 19:04:48 -0300

    Return active animes

--- src/functions/animes/get/handler.ts
+++ src/functions/animes/get/handler.ts
@@ -1,8 +1,14 @@
 import "source-map-support/register";
 
 import * as response from "@infra/http/response";
+import { Anime } from "@application/contracts/Anime";
+import { getActive as getActiveAnimes } from "@infra/repositories/animes";
+
 export const get = async () => {
   try {
+    const animes = await getActiveAnimes();
+
+    return response.OK<Anime[]>(animes);
   } catch (err) {
     console.log(err);
 
