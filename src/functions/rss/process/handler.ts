6ec4a0891e029171ffcc660cebc61d2ba24dacf7
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-11-13 13:52:19 -0300

    Configure validator

--- src/functions/rss/process/handler.ts
+++ src/functions/rss/process/handler.ts
@@ -1,14 +1,18 @@
 import "source-map-support/register";
 import { APIGatewayProxyEvent } from "aws-lambda";
+import { ZodError, ZodIssue } from "zod";
 
 import * as response from "@infra/http/response";
 import { getRssEpisodesFrom } from "@application/use_cases/getRssEpisodesFrom";
 import { addEpisodesInQueue } from "@application/use_cases/addEpisodesInQueue";
+import * as validate from "@application/validators/rss";
 
 export const process = async (event: APIGatewayProxyEvent) => {
-  const { date = new Date() } = event.queryStringParameters || {};
-
   try {
+    const { date = new Date() } = validate.date(
+      event.queryStringParameters || {}
+    );
+
     const episodes = await getRssEpisodesFrom(date.toString());
     if (episodes.length > 0) {
       await addEpisodesInQueue(episodes);
@@ -16,6 +20,10 @@ export const process = async (event: APIGatewayProxyEvent) => {
 
     return response.NoContent();
   } catch (err) {
+    if (err instanceof ZodError) {
+      return response.BadRequest<ZodIssue[]>(err.errors);
+    }
+
     console.log(err);
 
     return response.InternalServerError();
