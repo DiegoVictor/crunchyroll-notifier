cc447c5ec69bf69d0027f135a81fbb030b5fcaab
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-11-13 13:52:52 -0300

    Configure episodes validator

--- src/functions/queue/episodes/handler.ts
+++ src/functions/queue/episodes/handler.ts
@@ -1,4 +1,5 @@
 import { APIGatewayProxyEvent, Handler, SNSEvent } from "aws-lambda";
+import { ZodError, ZodIssue } from "zod";
 
 import { saveMany as saveEpisodes } from "@infra/repositories/episodes";
 import * as response from "@infra/http/response";
@@ -6,13 +7,16 @@ import { getFromMessages as getEpisodesFromMessages } from "@infra/repositories/
 import { deleteMessageBatch as removeMessagesFromQueue } from "@infra/services/queue";
 import { getFromRecords as getMessagesFromRecords } from "@infra/services/notification";
 import { mapFields } from "@application/parsers/episodes";
+import * as validate from "@application/validators/episodes";
 
 export const process: Handler<SNSEvent | APIGatewayProxyEvent> = async (
   event?
 ) => {
   try {
     if ("body" in event && event.body && event.body.length > 0) {
-      await saveEpisodes(JSON.parse(event.body).map(mapFields));
+      await saveEpisodes(
+        validate.episodes(JSON.parse(event.body)).map(mapFields)
+      );
     } else if ("Records" in event) {
       const messages = getMessagesFromRecords(event.Records);
 
@@ -26,6 +30,10 @@ export const process: Handler<SNSEvent | APIGatewayProxyEvent> = async (
 
     return response.NoContent();
   } catch (err) {
+    if (err instanceof ZodError) {
+      return response.BadRequest<ZodIssue[]>(err.errors);
+    }
+
     console.log(err);
 
     return response.InternalServerError();
