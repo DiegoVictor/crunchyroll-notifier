b5707ca983aea683a0f36313fb1a85a98d788bb4
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-11-17 18:49:57 -0300

    Get anime rss

--- src/infra/services/rss.ts
+++ src/infra/services/rss.ts
@@ -4,5 +4,12 @@ const feedburner = axios.create({
   baseURL: "http://feeds.feedburner.com",
 });
 
+const crunchyroll = axios.create({
+  baseURL: "https://www.crunchyroll.com",
+});
+
 export const getAnimesRss = async (): Promise<string> =>
   feedburner.get("/crunchyroll/rss/anime?lang=ptBR").then(({ data }) => data);
+
+export const getAnimeRssBySlug = async (slug: string): Promise<string> =>
+  crunchyroll.get(`/${slug}.rss`).then(({ data }) => data);
