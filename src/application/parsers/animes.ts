4b3ceab69c717d43e3ed94a2dcc566e72dd67911
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-11-18 18:37:41 -0300

    Add new anime fields

--- src/application/parsers/animes.ts
+++ src/application/parsers/animes.ts
@@ -7,13 +7,17 @@ export const slugify = (string: string) => string.replace(/(\W|_)/gi, "");
 export const mapFields = ({
   id = randomUUID(),
   title,
+  description = "",
   active = true,
+  image = "",
   topic = slugify(title),
 }: {
   [key: string]: any;
 }): Anime => ({
   id,
   title,
+  description,
+  image,
   active,
   topic,
 });
