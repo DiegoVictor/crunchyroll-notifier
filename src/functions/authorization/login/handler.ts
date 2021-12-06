9932890c32d9450581db5130e311a17c38425979
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-12-06 20:39:20 -0300

    Return validation error message

--- src/functions/authorization/login/handler.ts
+++ src/functions/authorization/login/handler.ts
@@ -5,6 +5,12 @@ import * as response from "@infra/http/response";
 export const login = async (event: APIGatewayProxyEvent) => {
   try {
   } catch (err) {
+    if (err instanceof ZodError) {
+      return response.BadRequest<ZodIssue[]>(err.errors);
+    }
+
+    console.log(err);
+
     return response.InternalServerError();
   }
 };
