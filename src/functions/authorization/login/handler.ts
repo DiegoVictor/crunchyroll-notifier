04affffedf1a9e4c80a3f9573d6f321d4f8cef9f
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-12-06 20:39:58 -0300

    Validate email and password

--- src/functions/authorization/login/handler.ts
+++ src/functions/authorization/login/handler.ts
@@ -4,6 +4,9 @@ import { APIGatewayProxyEvent } from "aws-lambda";
 import * as response from "@infra/http/response";
 export const login = async (event: APIGatewayProxyEvent) => {
   try {
+    const { email, password } = validate.authorization(
+      JSON.parse(event.body || "{}")
+    );
   } catch (err) {
     if (err instanceof ZodError) {
       return response.BadRequest<ZodIssue[]>(err.errors);
