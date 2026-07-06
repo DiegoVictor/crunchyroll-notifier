d485274019753c0072ab4c0c02dedc99ca29d99b
Diego Victor <diegovictorgonzaga@gmail.com>
2026-07-05 21:55:10 -0300

    Define factory for Episode with properties for title, serie, season, description, thumbnail, number, and publish dates

--- tests/utils/factory.ts
+++ tests/utils/factory.ts
@@ -1,6 +1,20 @@
 import { faker } from "@faker-js/faker";
 import { factory } from "factory-girl";
 
+factory.define(
+  "Episode",
+  {},
+  {
+    title: faker.lorem.words,
+    serie: faker.lorem.words,
+    season: () => String(faker.datatype.number()),
+    description: faker.lorem.paragraph,
+    thumbnail: faker.image.imageUrl,
+    number: () => String(faker.datatype.number()),
+    premiumPublishDate: () => faker.date.soon().getTime(),
+    freePublishDate: () => faker.date.soon().getTime(),
+  }
+);
 
 factory.define("Anime", {}, () => {
   const title = faker.lorem.words();
